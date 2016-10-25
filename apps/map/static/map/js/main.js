96940495724a4945e98ae3fd564a4d42d7755549
Ryan Pan <purexchaos@gmail.com>
2016-10-25 11:49:09 -0700

    added soda

--- apps/map/static/map/js/main.js
+++ apps/map/static/map/js/main.js
@@ -1,39 +1,58 @@
 $(document).ready(function(){
 
-var mapOptions = {
-    center: new google.maps.LatLng(37.3382,-121.8863),
-    zoom: 12,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-};
+    // $.get("https://data.sfgov.org/resource/cuks-n6tp.json", function(res){
+    //     console.log("in function");
+    //     console.log(res);
+    //     alert('hi');
+    // }, "json");
 
-var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-var acOptions = {
-  types: ['establishment']
-};
-var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),acOptions);
-autocomplete.bindTo('bounds',map);
-var infoWindow = new google.maps.InfoWindow();
-var marker = new google.maps.Marker({
-  map: map
-});
+    var consumer = new soda.Consumer("data.sfgov.org");
+    consumer.query()
+        .withDataset('cuks-n6tp')
+        .limit(100)
+        .getRows()
+            .on('success', function(rows){
+                for(var j=0; j <rows.length; j++)
+                    for(var i in rows)
+                        console.log(rows[i]);
+            })
+            .on('error', function(error){console.log(error);});
 
-google.maps.event.addListener(autocomplete, 'place_changed', function() {
-  infoWindow.close();
-  var place = autocomplete.getPlace();
-  if (place.geometry.viewport) {
-    map.fitBounds(place.geometry.viewport);
-  } else {
-    map.setCenter(place.geometry.location);
-    map.setZoom(17);
-  }
-  marker.setPosition(place.geometry.location);
-  infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
-  infoWindow.open(map, marker);
-  google.maps.event.addListener(marker,'click',function(e){
 
-    infoWindow.open(map, marker);
+    var mapOptions = {
+        center: new google.maps.LatLng(37.3382,-121.8863),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
 
-  });
-});
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var acOptions = {
+      types: ['establishment']
+    };
+    var autocomplete = new google.maps.places.Autocomplete(document.getElementById('autocomplete'),acOptions);
+    autocomplete.bindTo('bounds',map);
+    var infoWindow = new google.maps.InfoWindow();
+    var marker = new google.maps.Marker({
+      map: map
+    });
+
+    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      infoWindow.close();
+      var place = autocomplete.getPlace();
+      if (place.geometry.viewport) {
+        map.fitBounds(place.geometry.viewport);
+      } else {
+        map.setCenter(place.geometry.location);
+        map.setZoom(17);
+      }
+      marker.setPosition(place.geometry.location);
+      infoWindow.setContent('<div><strong>' + place.name + '</strong><br>');
+      infoWindow.open(map, marker);
+      google.maps.event.addListener(marker,'click',function(e){
+
+        infoWindow.open(map, marker);
+
+      });
+    });
 
 })
