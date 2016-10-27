932402932cd232f05a71f93ac932e9ec3d88bfdf
Ryan Pan <purexchaos@gmail.com>
2016-10-27 12:00:04 -0700

    added filter function

--- apps/map/static/map/js/attachlisteners.js
+++ apps/map/static/map/js/attachlisteners.js
@@ -102,19 +102,43 @@ $('#condo').click(function(){
         }
     }
 });
-//
-// $('#kidnapping').click(function(){
-//     if($('#kidnapping').is(':checked'))
-//         for(j = 0; j < dict['KIDNAPPING'].length; j++)
-//         {
-//             dict['KIDNAPPING'][j].setVisible(true);
-//         }
-//     else
-//     {
-//         for(j = 0; j < dict['KIDNAPPING'].length; j++)
-//         {
-//             dict['KIDNAPPING'][j].setVisible(false);
-//         }
-//     }
-// });
+
+$('#filterprice').click(function(){
+    if($('#sfhome').is(':checked'))
+        for(var i=0; i<dict['houses'].length;i++)
+            dict['houses'][i].setVisible(true);
+    if($('#condo').is(':checked'))
+        for(var i=0; i<dict['condo'].length;i++)
+            dict['condo'][i].setVisible(true);
+    if($('#rent').is(':checked'))
+        for(var i=0; i<dict['rent'].length;i++)
+            dict['rent'][i].setVisible(true);
+    var minrange = $('#minrange').val();
+    var maxrange = $('#maxrange').val();
+    if (minrange.length === 0)
+        minrange = 0;
+    if (maxrange.length === 0)
+        maxrange = Number.MAX_SAFE_INTEGER;
+    minrange = parseInt(minrange);
+    maxrange = parseInt(maxrange);
+    if(isNaN(minrange) || isNaN(maxrange))
+        return;
+    for(var i=0; i<dict['hprices'].length;i++)
+    {
+        if(dict['hprices'][i] > maxrange || dict['hprices'][i] < minrange)
+            dict['houses'][i].setVisible(false);
+    }
+    for(var i=0; i<dict['cprices'].length;i++)
+    {
+        if(dict['cprices'][i] > maxrange || dict['cprices'][i] < minrange)
+            dict['condo'][i].setVisible(false);
+    }
+    for(var i=0; i<dict['rprices'].length;i++)
+    {
+        if(dict['rprices'][i] > maxrange || dict['rprices'][i] < minrange)
+            dict['rent'][i].setVisible(false);
+    }
+})
+
+
 })
